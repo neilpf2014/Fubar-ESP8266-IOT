e5dcc03aa0b4447bce7634c9972f4364fd942d62
neilpf2014 <neilpf2014@gmail.com>
2016-07-19 01:49:45 -0400

    initial commit

---
+++ FUBAR_ESP_IOT_Example_1.ino
@@ -0,0 +1,22 @@
+/*	This is the standard Ardunio "Blink" code
+	We're using this a a hello world and to test our IDE setup and nodeMCU board
+	This blinks the LED on GPIO 6 defined as "LED_BUILTIN"
+*/
+
+#include "Arduino.h"
+
+void setup() 
+{
+	pinMode(LED_BUILTIN, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
+}
+
+// the loop function runs over and over again forever
+void loop() 
+{
+	digitalWrite(LED_BUILTIN, LOW);   // Turn the LED on (Note that LOW is the voltage level
+									  // but actually the LED is on; this is because 
+									  // it is acive low on the ESP-01)
+	delay(1000);                      // Wait for a second
+	digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED off by making the voltage HIGH
+	delay(2000);                      // Wait for two seconds (to demonstrate the active low LED)
+}
